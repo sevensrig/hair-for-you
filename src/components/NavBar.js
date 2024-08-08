1956df7f9a56d9ba63302994f0ae2c530888b804
sevensrig <srinivasan.sri@northeastern.edu>
2024-08-08 16:37:44 -0400

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function NavBar() {
+    return (
+        <header>
+            <h3>
+                
+            </h3>
+        </header>      
+    );
+}
+
+return NavBar;
