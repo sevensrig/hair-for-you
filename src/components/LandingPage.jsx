9d3b66e172273d1bad13c26a9ae4468aa528b7d0
sevensrig <srinivasan.sri@northeastern.edu>
2024-08-30 19:26:31 -0400

    Landing page designed out

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import LocationBox from './LocationBox';
 import './LandingPage.css'
 import { FaScissors } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 
 function LandingPage() {
+    const linkTo = useNavigate();
+    function handleBookButtonClick() {
+        linkTo('/book-an-appointment');
+    }
+
     return (
         <div className = 'landingPage' id = 'landingPage'>
             <div className = 'textContainer'>
@@ -12,7 +20,7 @@ function LandingPage() {
                 <h1 style = {{color: '#F19A3E'}}>Hair For You!</h1>
                 <p>We offer a multitude of different services for all your Hair Care needs!</p>
                 <h2>Ready to Book?</h2>
-                <button className = "bookButton">Book Appointment<FaScissors className='faScissors'/></button>
+                <button className = "bookButton" onClick = {handleBookButtonClick}>Book Appointment<FaScissors className='faScissors'/></button>
             </div>
             <div className='imageAndLocation'>
                 <div className='locationBox'><LocationBox /></div>
