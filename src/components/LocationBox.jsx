489972c3df9960f71a7c1ef20480bccab44bd805
sevensrig <srinivasan.sri@northeastern.edu>
2024-09-12 12:27:08 -0400

    Location box links to google maps directions

--- src/components/LocationBox.jsx
+++ src/components/LocationBox.jsx
@@ -4,17 +4,20 @@ import './LocationBox.css'
 
 function LocationBox() {
     return (
-        <div className='container'>
-            <div className='address'>
-                <h3>Address:</h3>
-                <p>1777 Main St</p>
-                <p>Suite 12</p>
-                <p>Tewksbury, MA, 01867</p>
-            </div>
-            <div className='pinContainer'>
-                <FaLocationDot className = "pin"/>
-            </div>  
-        </div>
+        <a href = 'https://www.google.com/maps/place/1777+Main+St,+Tewksbury,+MA+01876/@42.5913304,-71.2109927,17z/data=!3m1!4b1!4m6!3m5!1s0x89e3a13568a13a4d:0x816d0fafaf2a63f1!8m2!3d42.5913304!4d-71.2084178!16s%2Fg%2F11vrjsszq7?entry=ttu&g_ep=EgoyMDI0MDkxMC4wIKXMDSoASAFQAw%3D%3D'>
+            <div className='container'>
+                <div className='address'>
+                    <h3>Address:</h3>
+                    <p>1777 Main St</p>
+                    <p>Suite 12</p>
+                    <p>Tewksbury, MA, 01867</p>
+                </div>
+                <div className='pinContainer'>
+                    <FaLocationDot className = "pin"/>
+                </div>  
+            </div>            
+        </a>
+
     );
 }
 
