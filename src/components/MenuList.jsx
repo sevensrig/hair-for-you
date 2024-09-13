8da1e0f03c445a67f8f34862210603291cda8000
sevensrig <srinivasan.sri@northeastern.edu>
2024-09-13 16:46:11 -0400

    Smooth scroll on NavBar implemented

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
 import './MenuList.css';
 
 function MenuList(props) {
     return (
         <div className='menuList'>
             <ul className = {props.menuOpen}>
-                <li> <Link to="/services">Services</Link></li>
-                <li> <Link to="/contact">Contact</Link></li>
+                <li> <ScrollLink to="Services" spy={true} smooth={true} duration={500}>Services</ScrollLink></li>
+                <li> <ScrollLink to="contact" spy={true} smooth={true} duration={500}>Contact</ScrollLink></li>
                 <li> <Link to="/book-an-appointment">Book Appointment</Link> </li>
             </ul>
         </div>
