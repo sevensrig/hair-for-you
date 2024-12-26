29b1f54ea6016681cf5612997d1245dd71e6fe3c
sevensrig <srinivasan.sri@northeastern.edu>
2024-12-25 21:09:13 -0500

    Styling for computer choosing date page

--- src/pages/ChooseDate.jsx
+++ src/pages/ChooseDate.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './ChooseDate.css'
 import BookingCalendar from '../components/BookingCalendar';
 import BackButton from '../components/BackButton';
-import { Button } from 'react-scroll';
 import { FaArrowRight } from 'react-icons/fa6';
 
 
@@ -13,14 +12,16 @@ function ChooseDate(props) {
     const times = ['11:00 AM', '12:00PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM']
     return (
     <div className='chooseDate'>
-        <div className='bookingCalendarDiv'>
+        <div className='timesAndCalendarContainer'>
             <BookingCalendar />
-            <p>{props.selectedTime}</p>
-        </div>
+            <span style = {{marginLeft:"10px", display: "inline-block", width:"0.25rem", height:"27rem",
+                backgroundColor:"black", justifySelf:"center"}}/>
         <div className='timesContainer'>
             {times.map((time, index) => 
-            <button style = {props.selectedTime === time ? {backgroundColor:'#F19A3E'}:{}} onClick = {() => props.setSelectedTime(time)} key={index} className='timeBox'>{time}</button>
+            <button style = {props.selectedTime === time ? {backgroundColor:'#F19A3E'}:{}} 
+            onClick = {() => props.setSelectedTime(time)} key={index} className='timeBox'>{time}</button>
             )}
+        </div>
         </div>
         <div className = 'buttons' style={{display:"flex", justifyContent:'space-between', paddingLeft:'1rem', paddingRight:'1rem'}}>
         <BackButton onClick = {props.handleBackButton}/>
