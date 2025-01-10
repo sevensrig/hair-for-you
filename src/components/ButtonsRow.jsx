76bb89d8a2506482775dad2b2e19f105498f933a
sevensrig <srinivasan.sri@northeastern.edu>
2025-01-09 22:24:22 -0500

    Refactored buttons for next and previous page in booking page

---
+++ src/components/ButtonsRow.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import BackButton from './BackButton.jsx';
+import { FaArrowRight } from 'react-icons/fa6';
+import './ButtonsRow.css'
+
+function ButtonsRow(props) {
+    return (
+        <div className = 'buttons' style={{display:"flex", justifyContent:'space-between', paddingLeft:'1rem', paddingRight:'1rem'}}>
+        <BackButton onClick = {props.handleBackButton}/>
+        <button className = 'nextButton' onClick={props.handleNextButton}>
+            Next
+            <FaArrowRight className='arrowRight'/>
+            </button>
+        </div>
+    )
+}
+
+export default ButtonsRow;
