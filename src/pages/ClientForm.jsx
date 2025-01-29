41237b6ab0f13636024211ec307937f3705b20ed
sevensrig <srinivasan.sri@northeastern.edu>
2025-01-28 22:25:02 -0500

    Styled client forms for mobile and desktop

--- src/pages/ClientForm.jsx
+++ src/pages/ClientForm.jsx
@@ -7,13 +7,17 @@ import { FaArrowRight } from 'react-icons/fa6';
 function ClientForm(props) {
     return(
         <div className='formBox'>
-            <form>
-                <h3>{props.serviceChosen}</h3>
-                <h3>{props.dateChosen}</h3>
-                <h3>{props.timeChosen}</h3>
-                <label>Name:<input type="text" /></label>
-                <label>Email:<input type="text" /></label>
-            </form>
+                <form className='formBox'>
+                    <h3>Service Chosen: {props.serviceChosen}</h3>
+                    <h3>Date Chosen:{props.dateChosen}</h3>
+                    <h3>Time Chosen:{props.timeChosen}</h3>
+                    <div className='label'>
+                        <label>Name:<input type="text" /></label>
+                        <label>Email:<input type="text" /></label>    
+                    </div>
+                    
+                </form>
+
             <ButtonsRow handleBackButton = {props.handleBackButton} handleNextButton={props.handleNextButton} 
             nextButtonContent={<div>Next<FaArrowRight className='arrowRight'/></div>}/>
         </div>
